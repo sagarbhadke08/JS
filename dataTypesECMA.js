6d867c28a7fe7cd098f7382069366ad2b1b94bcc
sagarbhadke08 <sagarbhadke08@gmail.com>
2023-09-07 22:57:10 +0530

    dataTypesECMA

---
+++ dataTypesECMA.js
@@ -0,0 +1,7 @@
+console.log("hello");
+console.log(3+3); console.log(3)
+
+//null => It is a standalone value and datatype and its a representation value , suppose aapne wweather app create kiya
+// then u requested that mujhe temprature batao then if there is any dikkat in server then it sends Zero 0 then it is not good so there
+//i can use null
+// undefined => It is also a  
