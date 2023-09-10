9f94528d7ae289197dcfd19c8c7f7a2c244beffc
sagarbhadke08 <sagarbhadke08@gmail.com>
2023-09-10 23:32:47 +0530

    String Methods

---
+++ strings/first.js
@@ -0,0 +1,42 @@
+// * console.log("hello");
+//? console.log(3 + 3);
+// console.log(3);
+
+//null => It is a standalone value and datatype and its a representation value , suppose aapne wweather app create kiya
+// then u requested that mujhe temprature batao then if there is any dikkat in server then it sends Zero 0 then it is not good so there
+//i can use null
+// undefined => It is also a  type
+//? console.log("Sagar");
+
+const name = "Akash"
+
+const number = 50
+
+// console.log(name + number);
+//* In todays world it is outdated syntax so we use bacticks
+// so we write like this
+
+/*console.log(
+  `Hello this is printing using backticks which is String ${name} this is number ${number}`
+);*/
+
+const firstName = new String('SagarBhadke');
+// In this string is a object not an array
+// console.log(firstName[0]);
+console.log(firstName.length)
+console.log(firstName.toLowerCase())
+// console.log(firstName.\\\\\\\\\\\)
+const newString = firstName.substring(2,5)
+console.log(newString)
+
+const anotherString = firstName.slice(0,2
+    )
+console.log(anotherString)
+
+const newStringFor = "   My Name is Sagar   /"
+
+console.log(newStringFor.trim());
+
+const newStringFor1 = "   My Name is Sagar   /"
+
+console.log(newStringFor1);
