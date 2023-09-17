036abfb35cc3f0d23636e1386bf75ebd27fdb2ba
sagarbhadke08 <sagarbhadke08@gmail.com>
2023-09-17 21:28:29 +0530

    Added  Arrow functions Concepts

--- strings/Array.js
+++ strings/Array.js
@@ -1,28 +1,46 @@
-//?Array
-const myArray = [0,2,3,5,6];
-console.log(myArray);
+// //?Array
+// const myArray = [0,2,3,5,6];
+// console.log(myArray);
 
 //there another way also
-const myArray2 = new Array(0,1,2,5,3);
-console.log(myArray2);
-console.log(myArray2[5]);// undefined
+// const myArray2 = new Array(0,1,2,5,3);
+// console.log(myArray2);
+// console.log(myArray2[5]);// undefined
 
-//*Methods
+// //*Methods
 
-//?    Push
-//* It adds the value or push the value to the array
-myArray.push(8);
-console.log(myArray);
+// //?    Push
+// //* It adds the value or push the value to the array
+// myArray.push(8);
+// console.log(myArray);
 
-//?    unshift()
-console.log("-----------This is unshift method---------------");
-//* It inserts the value at the start of the array i.e. at 0th position index
+// //?    unshift()
+// console.log("-----------This is unshift method---------------");
+// //* It inserts the value at the start of the array i.e. at 0th position index
 
-myArray.unshift(214);// This is a value
-console.log(myArray);
+// myArray.unshift(214);// This is a value
+// console.log(myArray);
 
-//?   shift() 
-//There is a shift method where it removes the first index element its take example and we dont pass argument in shift method
-console.log("-----------This is shift method---------------");//it removed the 214
-myArray.shift();
-console.log(myArray);
+// //?   shift() 
+ //There is a shift method where it removes the first index element its take example and we dont pass argument in shift method
+// console.log("-----------This is shift method---------------");//it removed the 214
+// myArray.shift();
+// console.log(myArray);
+
+//? typeof and type conversion //* Right side to left side
+// var varq = "asa";
+// let no = Number(varq);
+// console.log(typeof no);
+// console.log(no);
+
+//* Arrow Functions
+
+let add = (a,b) => a+b;
+
+console.log(add(1,3));
+
+//? If we want to use {} curely braces then we need to use explicitly return
+//?Arrow functions are handy for simple actions, especially for one-liners. They come in two flavors:
+
+//? Without curly braces: (...args) => expression – the right side is an expression: the function evaluates it and returns the result. Parentheses can be omitted, if there’s only a single argument, e.g. n => n*2.
+//? With curly braces: (...args) => { body } – brackets allow us to write multiple statements inside the function, but we need an explicit return to return something.
